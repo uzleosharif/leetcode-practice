89714fbd96bf10e7709748106a465d0f1f40b309
uzleo <uzleo.sharif@gmail.com>
2022-12-08 11:50:10 +0000

    wip

--- roman2int.cpp
+++ roman2int.cpp
@@ -1,12 +1,16 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
+#include <map>
+#include <ranges>
 
 class Solution {
  public:
   int romanToInt(std::string) const;
 
  private:
-  const std::array<char, 7> alphabet_{'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+  const std::map<char, size_t> kAlphabetWeight{{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+                                               {'C', 100}, {'D', 500}, {'M', 1000}};
 };
 
 int Solution::romanToInt(std::string s) const {
@@ -17,19 +21,28 @@ int Solution::romanToInt(std::string s) const {
   int res{0};
 
   if (s.length() > 15 || s.length() < 1) {
-    throw std::invalid_argument{"passed input string length is not valid"};
+    throw std::invalid_argument{"passed input is not in valid range"};
   }
+  std::for_each(std::begin(s), std::end(s), [this, &res](const char &c) {
+    if (!kAlphabetWeight.contains(c)) {
+      throw std::invalid_argument{"passed input is not a roman numeral"};
+    }
 
-    return res;
+    res += kAlphabetWeight.at(c);
+  });
+
+  return res;
 }
 
 int main() {
   Solution sol{};
 
   try {
-    std::cout << sol.romanToInt("III") << "\n";
-    std::cout << sol.romanToInt("LVIII") << "\n";
-    std::cout << sol.romanToInt("MCMXCIV") << "\n";
+    // std::cout << sol.romanToInt("III") << "\n";
+    // std::cout << sol.romanToInt("LVIII") << "\n";
+    // std::cout << sol.romanToInt("MCMXCIV") << "\n";
+
+    std::cout << sol.romanToInt("XL") << "\n";
   } catch (const std::invalid_argument &ex) {
     std::cerr << ex.what() << '\n';
   }
